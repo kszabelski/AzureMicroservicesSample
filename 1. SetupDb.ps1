2f4b8d5db102cf3e89d5900cd32336c36d30c1bc
Krzysztof Szabelski <kszabelski@gmail.com>
2015-01-29 10:42:18 +0100

    Create Powershell deployment scripts

---
+++ 1. SetupDb.ps1
@@ -0,0 +1,48 @@
+﻿######### to customize ################
+## below name must be globaly uniqe ##
+$ordersDatabaseName = "ams_orders_szabl"
+
+#######################################
+$adminLogin = "jurassic"
+$adminPass = "jur@ssic_Pass1"
+
+Add-AzureAccount
+
+# Create database if not exist. It is created in first found database server
+$sqlServers = Get-AzureSqlDatabaseServer
+$sqlServer = $sqlServers | Select-Object -First 1
+ 
+if ($sqlServer -eq $null){
+    $sqlServer = New-AzureSqlDatabaseServer -Location "West Europe" -AdministratorLogin $adminLogin -AdministratorLoginPassword $adminPass
+    Write-Host "New SQL Server Created"
+    
+    $ruleName = [guid]::NewGuid().ToString()
+    $ipAddress  = (Invoke-WebRequest 'http://api.ipify.org').Content
+    
+    New-AzureSqlDatabaseServerFirewallRule -ServerName $sqlServer.ServerName -AllowAllAzureServices
+    New-AzureSqlDatabaseServerFirewallRule -ServerName $sqlServer.ServerName -RuleName $ruleName -StartIPAddress $ipAddress -EndIPAddress $ipAddress
+    Write-Host "Enabled server access from " $ipAddress
+}
+
+$orderDatabase = Get-AzureSqlDatabase -ServerName $sqlServer.ServerName  -DatabaseName $ordersDatabaseName -ErrorVariable a -ErrorAction silentlycontinue
+
+if ($orderDatabase -eq $null){
+   New-AzureSqlDatabase -ServerName $sqlServer.ServerName -DatabaseName $ordersDatabaseName #-Edition Basic # for some reason in Free Trial subscription i could create only "Web" edition
+   
+   Write-Host "Created new database"
+}
+
+$connectionString = "Server=tcp:{0}.database.windows.net;Database={1};User ID={2}@{0};Password={3};Trusted_Connection=False;Encrypt=True;Connection Timeout=30;" -f $sqlServer.ServerName, $ordersDatabaseName, $adminLogin, $adminPass
+
+Write-Host "Finished"
+Write-Host "==================="
+Write-Host "Now take connection string below and insert it into your web.configs of"
+Write-Host "- AzureMicroservicesSample.Web"
+Write-Host "- AzureMicroservicesSample.OrderService"
+Write-Host "under AzureMicroservicesSample.OrderService.Entities.OrdersContext key"
+Write-Host "==================="
+Write-Host $connectionString
+Write-Host "==================="
+
+Read-host
+
---
+++ 2. Deploy.ps1
@@ -0,0 +1,49 @@
+﻿# Pre requirements
+# - connection strings must be set to point your DB
+# - deployment package is created
+
+######### to customize ################
+## below names must be globaly uniqe ##
+$defaultStorageName = "amsdefaultstoreszabl" #just used for deployment
+$cloudServiceName = "amsszabl"
+
+#######################################
+
+$deploymentSlot = "Production"
+$packageUrl = 'E:\git\_testProjects\AzureMicroservicesSample\AzureMicroservicesSample.AzureCloudServiceDeployment\bin\Release\app.publish\AzureMicroservicesSample.AzureCloudServiceDeployment.cspkg'
+$cfgUrl = 'E:\git\_testProjects\AzureMicroservicesSample\AzureMicroservicesSample.AzureCloudServiceDeployment\bin\Release\app.publish\ServiceConfiguration.Cloud.cscfg'
+
+# TODO: where logs gointo when deploying this way?
+# next create shutdown and wake up scripts.
+
+#Add-AzureAccount
+
+$defaultStorage = Get-AzureStorageAccount -StorageAccountName $defaultStorageName -ErrorVariable a -ErrorAction silentlycontinue
+if ($defaultStorage -eq $null) {
+    New-AzureStorageAccount -StorageAccountName $defaultStorageName -Location "West Europe"
+    Write-Host "Default storage account created"
+}
+
+# AFAIK this default subsctiopion is used to upload package, that is later doployed to Cloud Service
+$azureSubscription = Get-AzureSubscription
+Set-AzureSubscription -SubscriptionName $azureSubscription.SubscriptionName -CurrentStorageAccountName $defaultStorageName
+Write-Host "Default storage account set for subscription"
+
+$service = Get-AzureService -ServiceName $cloudServiceName -ErrorVariable a -ErrorAction silentlycontinue
+if ($service -eq $null) {
+    New-AzureService -ServiceName $cloudServiceName -Location "West Europe"
+    Write-Host "New service created"
+}
+
+# Clear existing deployment
+$existingDeployment = Get-AzureDeployment -ServiceName $cloudServiceName -Slot $deploymentSlot -ErrorVariable a -ErrorAction silentlycontinue
+if ($existingDeployment -ne $null) {
+    Remove-AzureDeployment -ServiceName $cloudServiceName -Slot $deploymentSlot -Force
+    Write-Host "Removed existing deployment"
+}
+
+Write-Host "Uploading new package from $packageUrl"
+New-AzureDeployment -ServiceName $cloudServiceName -Slot $deploymentSlot -Package $packageUrl -Configuration $cfgUrl
+
+Write-Host "Finished"
+Read-host
