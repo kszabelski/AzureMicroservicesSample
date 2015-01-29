2ce2e400c760afe9cfb58ff570db216bf239d2b7
Krzysztof Szabelski <kszabelski@gmail.com>
2015-01-29 11:05:25 +0100

    Change db setup script to always create new server

--- 1. SetupDb.ps1
+++ 1. SetupDb.ps1
@@ -6,31 +6,24 @@ $ordersDatabaseName = "ams_orders_szabl"
 $adminLogin = "jurassic"
 $adminPass = "jur@ssic_Pass1"
 
-Add-AzureAccount
-
-# Create database if not exist. It is created in first found database server
-$sqlServers = Get-AzureSqlDatabaseServer
-$sqlServer = $sqlServers | Select-Object -First 1
- 
-if ($sqlServer -eq $null){
-    $sqlServer = New-AzureSqlDatabaseServer -Location "West Europe" -AdministratorLogin $adminLogin -AdministratorLoginPassword $adminPass
-    Write-Host "New SQL Server Created"
-    
-    $ruleName = [guid]::NewGuid().ToString()
-    $ipAddress  = (Invoke-WebRequest 'http://api.ipify.org').Content
-    
-    New-AzureSqlDatabaseServerFirewallRule -ServerName $sqlServer.ServerName -AllowAllAzureServices
-    New-AzureSqlDatabaseServerFirewallRule -ServerName $sqlServer.ServerName -RuleName $ruleName -StartIPAddress $ipAddress -EndIPAddress $ipAddress
-    Write-Host "Enabled server access from " $ipAddress
+if ((Get-AzureAccount) -eq $null) {
+	Add-AzureAccount
 }
+	
+# Create database
+$sqlServer = New-AzureSqlDatabaseServer -Location "West Europe" -AdministratorLogin $adminLogin -AdministratorLoginPassword $adminPass
+Write-Host "New SQL Server Created"
 
-$orderDatabase = Get-AzureSqlDatabase -ServerName $sqlServer.ServerName  -DatabaseName $ordersDatabaseName -ErrorVariable a -ErrorAction silentlycontinue
+$ruleName = [guid]::NewGuid().ToString()
+$ipAddress  = (Invoke-WebRequest 'http://api.ipify.org').Content
 
-if ($orderDatabase -eq $null){
-   New-AzureSqlDatabase -ServerName $sqlServer.ServerName -DatabaseName $ordersDatabaseName #-Edition Basic # for some reason in Free Trial subscription i could create only "Web" edition
+New-AzureSqlDatabaseServerFirewallRule -ServerName $sqlServer.ServerName -AllowAllAzureServices
+New-AzureSqlDatabaseServerFirewallRule -ServerName $sqlServer.ServerName -RuleName $ruleName -StartIPAddress $ipAddress -EndIPAddress $ipAddress
+Write-Host "Enabled server access from " $ipAddress
+
+New-AzureSqlDatabase -ServerName $sqlServer.ServerName -DatabaseName $ordersDatabaseName #-Edition Basic # for some reason in Free Trial subscription i could create only "Web" edition
    
-   Write-Host "Created new database"
-}
+Write-Host "Created new database"
 
 $connectionString = "Server=tcp:{0}.database.windows.net;Database={1};User ID={2}@{0};Password={3};Trusted_Connection=False;Encrypt=True;Connection Timeout=30;" -f $sqlServer.ServerName, $ordersDatabaseName, $adminLogin, $adminPass
 
